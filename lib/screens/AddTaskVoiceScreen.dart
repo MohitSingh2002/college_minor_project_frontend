2072d05790d69e0419639a8a7b8cfa56f18d9567
MohitSingh2002 <mohitsingh11082002@gmail.com>
2022-04-28 01:43:02 +0530

    Complete voice task functionality.

--- lib/screens/AddTaskVoiceScreen.dart
+++ lib/screens/AddTaskVoiceScreen.dart
@@ -1,8 +1,15 @@
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:college_minor_project_frontend/constants/AppStyle.dart';
 import 'package:college_minor_project_frontend/constants/Utils.dart';
+import 'package:college_minor_project_frontend/helpers/ApiService.dart';
+import 'package:college_minor_project_frontend/models/Task.dart';
+import 'package:college_minor_project_frontend/providers/EmailProvider.dart';
+import 'package:college_minor_project_frontend/providers/TasksProvider.dart';
 import 'package:college_minor_project_frontend/widgets/TextFieldCustomWithOutIcon.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 import 'package:speech_to_text/speech_to_text.dart';
 
 class AddTaskVoiceScreen extends StatefulWidget {
@@ -25,14 +32,22 @@ class _AddTaskVoiceScreenState extends State<AddTaskVoiceScreen> {
   void listen() async {
     if (!isListening) {
       bool available = await speechToText.initialize(
-        onStatus: (val) => print('onStatus: $val'),
+        onStatus: (val) {
+          print('onStatus: $val');
+          if (val == 'done') {
+            setState(() {
+              isListening = false;
+            });
+            speechToText.stop();
+          }
+        },
         onError: (val) => print('onError: $val'),
       );
       if (available) {
         setState(() => isListening = true);
         speechToText.listen(
           onResult: (val) => setState(() {
-            voiceText = val.recognizedWords;
+            voiceText += val.recognizedWords;
             taskController = TextEditingController(text: voiceText);
           }),
         );
@@ -78,7 +93,13 @@ class _AddTaskVoiceScreenState extends State<AddTaskVoiceScreen> {
                     maxLines: 10,
                     readOnly: true,
                     onTap: () {},
-                    validator: (value) {},
+                    validator: (value) {
+                      if (value != null) {
+                        if (value.isEmpty) {
+                          return 'Please tap mic button to record your task';
+                        }
+                      }
+                    },
                   ),
                   SizedBox(
                     height: Utils.width(context) / 30.0,
@@ -100,6 +121,77 @@ class _AddTaskVoiceScreenState extends State<AddTaskVoiceScreen> {
                             radius: 30.0,
                           ),
                         ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: Utils.width(context) / 30.0,
+                  ),
+                  TextFieldCustomWithOutIcon(
+                    controller: dateController,
+                    keyboardType: TextInputType.text,
+                    hintText: 'Pick completion date and time',
+                    readOnly: true,
+                    onTap: () {
+                      DatePicker.showDateTimePicker(
+                          context,
+                          theme: DatePickerTheme(
+                            doneStyle: TextStyle(
+                              color: AppStyle.red,
+                            ),
+                          ),
+                          onConfirm: (dateTime) {
+                            setState(() {
+                              dateController = TextEditingController(text: DateFormat('d/M/yyyy hh:mm a').format(dateTime));
+                            });
+                          }
+                      );
+                    },
+                    validator: (value) {
+                      if (value != null) {
+                        if (value.isEmpty) {
+                          return 'Can\'t be empty';
+                        }
+                      }
+                    },
+                  ),
+                  SizedBox(
+                    height: Utils.width(context) / 10.0,
+                  ),
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (formKey.currentState!.validate()) {
+                          setState(() {
+                            isLoading = true;
+                          });
+                          Task task = Task.addTask(
+                            email: Provider.of<EmailProvider>(context, listen: false).getEmail(),
+                            task: taskController.text.toString(),
+                            isCompleted: 'false',
+                            estimatedCompletionTime: dateController.text.toString(),
+                          );
+                          ApiService().addTask(task).then((value) {
+                            Provider.of<TasksProvider>(context, listen: false).addTask(value);
+                            Utils.showToast(message: 'Task Added Successfully');
+                            Navigator.pop(context);
+                          }).catchError((onError) {
+                            print('onError : ${onError}');
+                            setState(() {
+                              isLoading = false;
+                            });
+                            Utils.showToast(message: 'Some error occurred, please try again later');
+                          });
+                        }
+                      },
+                      child: Text(
+                        'Save',
+                        style: TextStyle(
+                          color: AppStyle.white,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        primary: AppStyle.red,
                       ),
                     ),
                   ),
